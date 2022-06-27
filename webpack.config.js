3fc5f0e1d5becb17ffe1ddf71438468d9c24979c
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 22:21:09 +0200

    Adding missing copyrights
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
-/*********************************************************************
-* Copyright (c) 2018 Red Hat, Inc.
-*
-* This program and the accompanying materials are made
-* available under the terms of the Eclipse Public License 2.0
-* which is available at https://www.eclipse.org/legal/epl-2.0/
-*
-* SPDX-License-Identifier: EPL-2.0
-**********************************************************************/
-
+/*******************************************************************************
+ * Copyright (C) 2021-2022 KAIROS DS
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
